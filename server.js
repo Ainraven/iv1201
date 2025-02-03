64fa782025c6ce65c4945324d917608ccc8f628b
Ensco <duttavanshu@hotmail.com>
2025-02-03 13:26:49 +0100

    Basic express implementation
    basic express implementation where i app can listen to a server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config({path: `${process.cwd()}/.env`});
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.json({
+        status:"success",
+        message:"you connected woo"
+    })
+})
+
+const PORT = process.env.SERVER_PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server is up and running!" , PORT)
+})
