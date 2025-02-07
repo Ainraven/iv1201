5fdf3bf1b0add19083efa0141cc5f378c6c607cc
Vladimir Li <ainrave.ilsley@outlook.com>
2025-02-07 20:22:42 +0100

    UserDTO and Validators
    Added file Validators that has a bunch of functions to validate different types of data. The types chose are relevant for UserDTO. Added validationa and constructor to UserDTO. Next to implement is applicationDTO.

--- model/user/userDTO.js
+++ model/user/userDTO.js
@@ -2,3 +2,40 @@
  * Data Transferable Object for a user of any kind
  * @DTO
  */
+
+const Validator = require('../../util/validators')
+
+class UserDTO {
+
+    /**
+     * 
+     * @param {number} id 
+     * @param {string} username 
+     * @param {string} password 
+     * @param {string} firstName 
+     * @param {string} lastName 
+     * @param {number} personalNumber 
+     * @param {string} email 
+     * @param {integer} role 
+     */
+    constructor(id, username, password, firstName, lastName, personalNumber, email, role){
+        Validator.isNotNegativeInteger(id)
+        Validator.isValidUsername(username)
+        Validator.isStrongPassword(password)
+        Validator.isValidName(firstName)
+        Validator.isValidName(lastName)
+        Validator.isPersonalNumber(personalNumber)
+        Validator.isValidEmail(email)
+        Validator.isNotNegativeInteger(role)
+        this.id  = id
+        this.username = username
+        this.password = password
+        this.firstName = firstName
+        this.lastName = lastName
+        this.personalNumber = personalNumber
+        this.email = email
+        this.role = role
+    }
+}
+
+module.exports = UserDTO
