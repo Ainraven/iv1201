85d6ac0077566a42880083e6298abd92a6d011af
Ensco <duttavanshu@hotmail.com>
2025-02-10 14:00:52 +0100

    commented files
    commented userDAO.js and databaseTest.js for clarity

--- integration/UserDAO.js
+++ integration/UserDAO.js
@@ -4,16 +4,23 @@ const { initModels } = require('../models/init-models')
 const databaseConfigPath = './config/database.js'
 const Sequelize = require ('sequelize')
 
+/* 
+Class containing constructor for the DAO and its related methods. It is responsible for calls to the database.
+CLS-hooked is used to enable easier use of transactions, but its not completely implemented in this iteration
+*/
+
 class UserDAO {
     constructor() {
-        // const name = cls.createNamespace('app-db');
+        // const name = cls.createNamespace('iv1201-db');
         // Sequelize.useCLS(name);
         this.database = require (databaseConfigPath)
         const models = initModels(this.database);
          this.Person = models.person;
     
     };
-
+    /*
+    method used to confirm that a connection has been established
+    */ 
      async connectToDB(){
     try {
         await this.database.authenticate();
@@ -22,6 +29,11 @@ class UserDAO {
         console.error('Unable to connect to the database:', error);
       }
     }
+
+    /*
+    Querries the database where @param username is used to check for username in the table. 
+    Returns the whole row
+    */ 
 
     async findPerson(username){
         try{
@@ -35,10 +47,11 @@ class UserDAO {
                 console.log("failed to find person", err)
             }
     }
-    //limit to 10 for now
+    //Used to all persons in the person table
+
     async findAllPersons(){
         try {
-            const people = await this.Person.findAll({limit:10})
+            const people = await this.Person.findAll({limit:10}) //limit to 10 for now, change with the limit parameter in find all
           //console.log('All users:', JSON.stringify(people, null, 2));
             return people;
             
@@ -47,6 +60,11 @@ class UserDAO {
         }
     }
 
+    /**
+     * 
+     * @param ID is used to match with the person_id in person table 
+     * @returns the whole row with said person
+     */
     async findUserById(ID){
         try{
             const person = await this.Person.findAll({
