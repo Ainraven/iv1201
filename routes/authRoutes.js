85491a0082215edccf51dcafb514edefac71a132
Vladimir Li <ainrave.ilsley@outlook.com>
2025-03-08 15:08:08 +0100

    feat: navigation bare bones
    Adds a header to all pages with navigation to be able go to pages from ui

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ const contr = new Controller()
 router.get('/login', (req, res) => {res.render('loginView')})
 router.post('/login/api', (req, res) => contr.login(req, res))
 
-router.get('/signup', (req, res) => {res.render('signup')})
+router.get('/signup', (req, res) => {res.render('signupView')})
 router.post('/signup/api', (req, res) => contr.signup(req, res))
 
 module.exports = router
