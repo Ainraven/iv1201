ab5797f1a649240e232ded9836e81ebe6e6f6995
Vladimir Li <ainrave.ilsley@outlook.com>
2025-03-08 16:36:56 +0100

    docs: adds jsdocs for functions in headerScript

--- api/public/js/headerScript.js
+++ api/public/js/headerScript.js
@@ -1,3 +1,9 @@
+/**
+ * Loads buttons in the header navigation that are relevant for that specific user. 
+ * If a person is not logged in, they will see "login" and "signup"
+ * IF a person is logged in, they will see "log out" and "my profile"
+ * If they are a recruiter, they will see "applications"
+ */
 async function loadNavigation() {
     try {
         var navlist = document.getElementById("navlist")
@@ -8,7 +14,6 @@ async function loadNavigation() {
             createNavElement("/auth/signup", "Sign up")
         } else {
             const decodedToken = parseJwt(token)
-            console.log(decodedToken)
             if(decodedToken.role === 1) {
                 createNavElement("/applications", "Applications")
             }
@@ -21,6 +26,11 @@ async function loadNavigation() {
     }
 }
 
+/**
+ * Adds an element to the navigation bar
+ * @param {String} endpoint link to a page 
+ * @param {String} text describes what the button does
+ */
 async function createNavElement(endpoint, text) {
     var element = document.createElement('li')
     var elementLink = document.createElement('a')
@@ -30,6 +40,14 @@ async function createNavElement(endpoint, text) {
     navlist.appendChild(element)
 }
 
+/**
+ * Parses jwt token to use variables inside:
+ *  - username
+ *  - role (1 or 2)
+ *  - id (of a person)
+ * @param {*} token 
+ * @returns 
+ */
 function parseJwt(token) {
     try {
         return JSON.parse(atob(token.split(".")[1]))
